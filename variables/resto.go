88abf4e52fa4a0ee86674d71e03c322c200bc8bc
Esleban Perez <eslebanperez@gmail.com>
2024-06-25 21:23:53 -0600

    Manejo de variables

---
+++ variables/resto.go
@@ -0,0 +1,25 @@
+package variables
+
+import (
+	"fmt"
+	"time"
+)
+
+var Nombre string
+var Estado bool
+var Sueldo float32
+var Fecha time.Time
+var variable string
+
+func RestoVariables() {
+	Nombre = "Pedro"
+	Estado = true
+	Sueldo = 1577.00
+	Fecha = time.Now().UTC()
+	fmt.Println(Nombre)
+	fmt.Println(Estado)
+	fmt.Println(Sueldo)
+	fmt.Println(Fecha)
+	variable = "alguito"
+	fmt.Println(variable)
+}
